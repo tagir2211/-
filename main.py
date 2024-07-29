a4b397173602aeba76bf646149b85cd04107d178
tagir2211 <tagir2211@gmail.com>
2024-07-29 21:51:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+    # 1st program
+    print(9**0.5*5)
+    # 2nd program
+    print(9.99>9.98 and 1000 != 1000.1)
+    # 3rd program
+    print(1234//10%(100)+5678//10%(100))
+    # 4th program
+    print(int(13.42) == 42.13 *100% 100 or int(42.13) == 13.42 *100% 100)
